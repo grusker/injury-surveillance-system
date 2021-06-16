0cd4b84aeabacb23d794fc2d82d40a4c83bbb20c
Grusker <grusker@github.com>
2021-06-16 13:20:11 +0300

    First commit

---
+++ injurysurveillancesystem/src/main/java/com/bb/injurysurveillancesystem/InjurysurveillancesystemApplication.java
@@ -0,0 +1,13 @@
+package com.bb.injurysurveillancesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InjurysurveillancesystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InjurysurveillancesystemApplication.class, args);
+	}
+
+}
